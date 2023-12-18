915a3e93a4bc89ba4b5124d9a6b80fe47f288133
Tomoki <tomoki.taniguchi@rakus-partners.co.jp>
2023-12-18 15:25:16 +0900

    first commit

---
+++ src/node/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+const port = 自分の社員番号;
+
+const cors = require("cors");
+app.use(cors());
+
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "x", // PostgreSQLのユーザー名に置き換えてください
+  host: "localhost",
+  database: "x", // PostgreSQLのデータベース名に置き換えてください
+  password: "x", // PostgreSQLのパスワードに置き換えてください
+  port: 5432,
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+app.get("/customers", async (req, res) => {
+  try {
+    const customerData = await pool.query("SELECT * FROM customers");
+    res.send(customerData.rows);
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/add-customer", async (req, res) => {
+  try {
+    const { companyName, industry, contact, location } = req.body;
+    const newCustomer = await pool.query(
+      "INSERT INTO customers (company_nam, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
+      [companyName, industry, contact, location]
+    );
+    res.json({ success: true, customer: newCustomer.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false });
+  }
+});
+
+app.use(express.static("public"));
---
+++ test/cypress/test.cy.js
@@ -0,0 +1,31 @@
+describe('顧客情報入力フォームのテスト', () => {
+  it('顧客情報を入力して送信し、成功メッセージを確認する', () => {
+    cy.visit('/nishi/customer/add.html'); // テスト対象のページにアクセス
+    cy.window().then((win) => {
+      // windowのalertをスタブ化し、エイリアスを設定
+      cy.stub(win, 'alert').as('alertStub');
+    });
+
+    // テストデータの読み込み
+    cy.fixture('customerData').then((data) => {
+      // フォームの入力フィールドにテストデータを入力
+      const uniqueContactNumber = `03-${Math.floor(1000 + Math.random() * 9000)}-${Math.floor(1000 + Math.random() * 9000)}`;
+      cy.get('#companyName').type(data.companyName);
+      cy.get('#industry').type(data.industry);
+      cy.get('#contact').type(uniqueContactNumber);
+      cy.get('#location').type(data.location);
+    });
+
+    // フォームの送信
+    cy.get('#customer-form').submit();
+
+    cy.get('@alertStub').should('have.been.calledOnceWith', '顧客情報が正常に保存されました。');
+
+    // フォームがリセットされたことを確認
+    cy.get('#companyName').should('have.value', '');
+    cy.get('#industry').should('have.value', '');
+    cy.get('#contact').should('have.value', '');
+    cy.get('#location').should('have.value', '');
+    cy.wait(5000);
+  });
+});
