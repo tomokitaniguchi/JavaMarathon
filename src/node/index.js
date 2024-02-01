6cadfaba514f6382311e122cb12779cffbbd6ecf
Tomoki <tomoki.taniguchi@rakus-partners.co.jp>
2024-02-01 17:51:14 +0900

    案件機能を実装

--- src/node/index.js
+++ src/node/index.js
@@ -9,10 +9,10 @@ app.use(cors());
 
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: 'user_tomoki_taniguchi', // PostgreSQLのユーザー名に置き換えてください
-  host: 'localhost',
-  database: 'db_tomoki_taniguchi', // PostgreSQLのデータベース名に置き換えてください
-  password: 'pass', // PostgreSQLのパスワードに置き換えてください
+  user: 'user_3449', // PostgreSQLのユーザー名に置き換えてください
+  host: 'db',
+  database: 'crm_3449', // PostgreSQLのデータベース名に置き換えてください
+  password: 'pass_3449', // PostgreSQLのパスワードに置き換えてください
   port: 5432,
 });
 
@@ -23,7 +23,7 @@ app.listen(port, () => {
 // 一覧表示画面の処理
 app.get("/customers", async (req, res) => {
   try {
-    const customerData = await pool.query("SELECT * FROM customers");
+    const customerData = await pool.query("SELECT * FROM customers ORDER BY customer_id");
     res.send(customerData.rows);
   } catch (err) {
     console.error(err);
@@ -97,10 +97,10 @@ app.post("/edit/:customerId", async (req, res) => {
 // 新規案件情報を登録する処理
 app.post("/add-case", async (req, res) => {
   try {
-    const { caseName, caseStatus, expectedRevenue, representative } = req.body;
+    const { caseName, caseStatus, expectedRevenue, representative, customerId} = req.body;
     const newCase = await pool.query(
-      "INSERT INTO cases (case_name,case_status,expected_revenue,representative) VALUES ($1,$2,$3,$4) RETURNING *",
-      [caseName, caseStatus, expectedRevenue, representative]
+      "INSERT INTO cases (case_name,case_status,expected_revenue,representative,customer_id) VALUES ($1,$2,$3,$4,$5) RETURNING *",
+      [caseName, caseStatus, expectedRevenue, representative, customerId]
     );
     res.json({ success: true, customer: newCase.rows[0] });
   } catch (err) {
@@ -110,12 +110,59 @@ app.post("/add-case", async (req, res) => {
 });
 
 // 案件一覧表示画面の処理
-app.get("/cases", async (req, res) => {
+app.get("/cases/:customerId", async (req, res) => {
   try {
-    const caseData = await pool.query("SELECT * FROM cases");
+    const customerId = req.params.customerId; // URL パラメータから customerId を取得
+    const caseData = await pool.query(
+      "SELECT case_id, case_name, case_status, expected_revenue, representative FROM cases WHERE customer_id = $1 ORDER BY case_id",
+    [customerId]
+    ); // クエリの WHERE 句に customerId を使用
     res.send(caseData.rows);
   } catch (err) {
     console.error(err);
     res.send("Error " + err);
+  }
+});
+
+// 案件詳細表示画面の処理
+app.get("/case/:customerId", async (req, res) => {
+  try {
+    const caseId = req.params.customerId; // URL パラメータから customerId を取得
+    const customerData = await pool.query(
+      "SELECT case_id, case_name, case_status, expected_revenue, representative, customer_id, created_date, updated_date FROM cases WHERE case_id = $1",
+     [caseId]); // クエリの WHERE 句に customerId を使用
+    res.send(customerData.rows);
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+// 案件情報を削除する処理
+app.delete("/deleteCase/:caseId", async (req, res) => {
+  try {
+    const caseId = req.params.caseId; 
+    const deleteCase = await pool.query(
+      "DELETE FROM cases WHERE case_id = $1",
+      [caseId]);
+    res.json({ success: true, customer: deleteCase.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false });
+  }
+});
+
+// 顧客情報を変更する処理
+app.post("/editCase/:caseId", async (req, res) => {
+  try {
+    const { caseId } = req.params; 
+    const { caseName, caseStatus, expectedRevenue, representative } = req.body;
+    const updateCase = await pool.query(
+      "UPDATE cases SET case_name = $1, case_status = $2, expected_revenue = $3, representative = $4, updated_date = CURRENT_TIMESTAMP WHERE case_id = $5",
+      [caseName, caseStatus, expectedRevenue, representative, caseId]);
+    res.json({ success: true, customer: updateCase.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false });
   }
 });
